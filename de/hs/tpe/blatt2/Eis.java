e05337cd17df35aa531b1a71a7845cd3559cff67
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-04-22 15:48:10 +0200

    added files  Eis, Eisdiele.  ennums Sorte, Becher

---
+++ de/hs/tpe/blatt2/Eis.java
@@ -0,0 +1,42 @@
+package de.hs.tpe.blatt2;
+
+public class Eis {
+
+	public Eis() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO 
+
+	}
+	String name(){ //eisbechernamen 
+		return null;
+	}
+	double preis(Eis becher){
+		return 0.0;
+	}
+	String behaeltniss(){//wafel becher glas etc
+		return null;
+	}
+	String art(){// bällchen spagetti eis etc
+		return null;
+	}
+	String []sorten(){
+		return null;
+	}
+	String []extras(){	// sahne streusel sauce etc
+		return null;
+	}
+	void vorbereiten(){
+		//behältniss nehmen
+	}
+	void fuellen(){// behältniss mit sorte nach art
+		
+	}
+	void dekorieren(){}
+	// konsolen angabe soll immermit satz zeichen enden.
+	
+
+
+}
---
+++ de/hs/tpe/blatt2/Eisdiele.java
@@ -0,0 +1,15 @@
+package de.hs.tpe.blatt2;
+
+class Eisdiele extends Eis {
+
+	public Eisdiele() {
+		// TODO Auto-generated constructor stub
+	}
+	public void bestellen(String typ){}
+	
+	void begruessen(){}
+	void Kassieren(){}
+	void verabschieden(){}
+	void endschuldigen(){}
+
+}
