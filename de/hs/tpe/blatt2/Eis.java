98e0d6291ade18c0e10c235a01feb5f5cb5b73fd
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-04-23 17:14:53 +0200

    input output methods work most liekly no more bugs

--- de/hs/tpe/blatt2/Eis.java
+++ de/hs/tpe/blatt2/Eis.java
@@ -1,62 +1,78 @@
 package de.hs.tpe.blatt2;
-
+import java.util.*;
 
 
 public class Eis {
 
 	public Eis() {
 	
-		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
-		Eis myEis= new Eis();
-		myEis.speisekate();
-
+	Eisdiele venzia= new Eisdiele();
+	venzia.bestellen(" ");
+		
 	}
 	
-	
-	String gefaes,Eisbecher,form;
+	//fields
+	 String gefaes,Eisbecher,form;
 	double preis;
 	String[]eisSorten;
 	String []extras;
 	
 
-	double preis(){	// getter methode????
+	double preis(){	// getter
 	return preis;
 	}
 	
-	
+	//getter
 	String behaeltniss(){
 		return gefaes;
 	}
-	String art(){// bällchen spagetti eis etc
+	//getter
+	String art(){
 		return form;
 	}
+	//getter
 	String []sorten(){
 		return eisSorten;
 	}
-	String []extras(){	// sahne streusel sauce etc
+	// getter
+	String []extras(){	
 		return extras;
 	}
+	//output methods
 	void vorbereiten(){
-		//behältniss nehmen
+	System.out.println("Der Eisverkäufer, säubert den eislöffel und  nimmt "+gefaes+".");
 	}
-	void fuellen(){// behältniss mit sorte nach art
-		
+	void fuellen(){	// output method
+		System.out.println("");
+		System.out.print(gefaes+ " wird mit einer ");
+		for (String s:eisSorten){
+			System.out.print(form+" "+s+", ");
+		}
+		System.out.println("gefüllt.");
 	}
-	void dekorieren(){}
-	// konsolen angabe soll immermit satz zeichen enden.
+	void dekorieren(){	// output method
+		System.out.print("das eis wird mit ");
+		for (String s:extras){
+			System.out.print(s+", ");
+		}
+		System.out.print("dekoriert.");
+		System.out.println(" ");
+	}
 	
-	private void rezept(String bestellung){
+	// huge setter method. fills all fields according to the "recipe" string matching the user input.
+	//if user input invalid Eisbecher is "no", leafes all fields at null.
+	protected void rezept(String bestellung){
 		if( bestellung.equals("Spagetieis")){
 		 
 		 gefaes="Schale";
 		 Eisbecher="Spagetieis";
-		 form="Durch die Nudelpresse gepresst";
+		 form="Durch die Nudelpresse gepresstes";
 			preis=3.90;
 			eisSorten=new String[1];
-			eisSorten[0]="vanille";
+			eisSorten[0]="vanille eis";
 			//String []eisSorten={"Vanille"};
 			extras=new String[3];
 			extras[0]="Sahne";
@@ -68,7 +84,7 @@ public class Eis {
 			//sorte= "Nuss, Vanille, Schokolade";
 			gefaes=	"Kelch";
 			Eisbecher="Nussbecher";
-			form="kugeln";
+			form="kugel";
 			preis=5.50;
 			eisSorten=new String[3];
 			eisSorten[0]="Nuss";
@@ -77,13 +93,13 @@ public class Eis {
 			
 			extras=new String[2];
 			extras[0]="schlagsahne";
-			extras[0]="Wallnüsse";
+			extras[1]="Wallnüsse";
 		}	
 		else if (bestellung.equals("Bananensplit")){
 			
 			gefaes=	"lange Schale";
 			Eisbecher="Bananensplit";
-			form= "kugeln";
+			form= "kugel";
 			preis= 4.50;
 			eisSorten=new String[2];
 			eisSorten[0]="Banane";
@@ -95,39 +111,73 @@ public class Eis {
 		
 		}
 		else if (bestellung.equals("Erdbeerbecher")){
-		/*	sorte=
-			gefaes=	
-			Eisbecher=
-			form=
-			preis=
-			eisSorten=new String[1];
+		
+			gefaes=	"Kelch";
+			Eisbecher="Erdbeerbecher";
+			form="Baellchen";
+			preis=5.00;
+			eisSorten=new String[2];
+			eisSorten[0]="Erdbeer";
+			eisSorten[1]="vanille";
 			extras=new String[3];
-		*/	
+			extras[0]="Erdbeeren";
+			extras[1]="Schlagsahne";
+			extras[2]="Erdbeersauce";
+		
 		}		else if (bestellung.equals("Pinoccio")){
-		/*	sorte=
-			gefaes=	
-			Eisbecher=
-			form=
-			preis=
-			eisSorten=new String[1];
-			extras=new String[3];
-		*/	
+		
+			gefaes=	"Teller";
+			Eisbecher="Pinoccio Eis";
+			form="Kugel";
+			preis=3.50;
+			eisSorten=new String[3];
+			eisSorten[0]="Bannane";
+			eisSorten[1]="Erdbeer";
+			eisSorten[2]="Schokolade";
+			extras=new String[2];
+			extras[0]="Waffelroellchen";
+			extras[1]="Smarties";
+			
 		}
-		else if (bestellung.equals("Pfirsichbecher")){
-		/*	sorte=
-			gefaes=	
-			Eisbecher=
-			form=
-			preis=
-			eisSorten=new String[1];
-			extras=new String[3];
-		*/	
+		else if (bestellung.equals("Pfirsichmelba")){
+			
+			gefaes=	"Kelch";
+			Eisbecher="Pfirsichbecher";
+			form= "kugel";
+			preis=5.00;
+			eisSorten=new String[3];
+			eisSorten[0]="Vannille";
+			eisSorten[1]="Mango";
+			eisSorten[2]="Pfirsich";
+			extras=new String[2];
+			extras[0]="Pfirsiche";
+			extras[1]="Schlagsahne";
+				
 		}
-		else{}
+	else if (bestellung.equals("Exotikbecher")){
+			
+			gefaes=	" grosser Kelch";
+			Eisbecher="Exotikbecher";
+			form= "kugel";
+			preis=6.00;
+			eisSorten=new String[4];
+			eisSorten[0]="Melone";
+			eisSorten[1]="Mango";
+			eisSorten[2]="Pfirsich";
+			eisSorten[3]= "Cocus";
+			extras=new String[3];
+			extras[0]="tropische Früchte";
+			extras[1]="Schlagsahne";
+			extras[2]="Kiwisauce";
+				
+		}
+		else{ Eisbecher= "no";
+			
+		}
 	
 	}
 	
-	String speisekate(){ //contains the ennums Becher print them all in correct capitalisation
+		void speisekate(){ //contains the ennums Becher print them all in correct capitalisation prints the menue.
 		
 		for (Becher i: Becher.values()){
 			String becher =i.toString();
@@ -138,7 +188,7 @@ public class Eis {
 			System.out.println(be);		
 		}
 			
-			return null;
+		
 		}
 	
 }
--- de/hs/tpe/blatt2/Eisdiele.java
+++ de/hs/tpe/blatt2/Eisdiele.java
@@ -5,48 +5,71 @@ class Eisdiele extends Eis {
 	public Eisdiele() {
 		// TODO Auto-generated constructor stub
 	}
-	Eis dummValue= new Eis();
-	Eisdiele deinBecher= new Eisdiele();
-	Becher customerOrder;
-	
-	public void bestellen(String typ){	// most important method theoretichally
-	deinBecher.bestellen();
-	} 
+	//Eis dummValue= new Eis();
+//	Eisdiele deinBecher= new Eisdiele();
+	//Becher customerOrder;
+
+	//Eisdiele lokal= new Eisdiele();
+	Eis myEis= new Eis();
+	public void bestellen(String typ){	// prepares the ice cream sunday	
+	bestellen();
+	}
+
 	private String bestellen(){// practically the most important one
+	
+		Scanner input=new Scanner(System.in);
+		System.out.println("wir haben");
+		myEis.speisekate();
+		begruessen();
+		String bestellung= input.nextLine();
+		myEis.rezept(bestellung);
+		
+		if (myEis.Eisbecher.equals("no")){
+			endschuldigen();
+			}
+		else{
+			myEis.vorbereiten();
+			myEis.fuellen();
+			myEis.dekorieren();
+			kassieren();
+		}
+		verabschieden();
+		
+		input.close();
+	
+	
 		return null;
 	}
 	
-	private boolean isValid(String order){
-		order=order.toLowerCase();
-		boolean ret=false;
-		for (Becher i: Becher.values()){
-			String becher =i.toString();
-			becher=becher.toLowerCase();
-			if (becher==order){
-				ret=true;
-			}
-		}
-		return ret;
-	}
+//	private boolean isValid(String order){
+//		order=order.toLowerCase();
+//		boolean ret=false;
+//		for (Becher i: Becher.values()){
+//			String becher =i.toString();
+//			becher=becher.toLowerCase();
+//			if (becher==order){
+//				ret=true;
+//			}
+//		}
+//		return ret;
+//	}
 
 	
-	void begruessen(){
-		Scanner input= new Scanner(System.in);
+	void begruessen(){	// outputt method
+		
 		System.out.println("Guten tag welchen Eisbecher möchten sie bestellen?");
-		String order=input.nextLine();
-		//setCustomerOrder(order);
-		input.close();
+	
 	}
 	
 
-	void Kassieren(){
-		int a=0;
-		System.out.println("Das macht"+a+"€ bitte.");
+	void kassieren(){	// outputmethod
+		double kosten= myEis.preis();
+		System.out.println("Das macht"+kosten+"€ bitte.");
 	}
-	void verabschieden(){
+	void verabschieden(){//output method
 		System.out.println("Aufwidersehen lassen sie sich ihr Eis schmecken");
 	}
-	void endschuldigen(){
+	void endschuldigen(){	// outputmethod
 		System.out.println("Bedaure diesen eissbecher gibt es bei uns nicht");
 	}
 
