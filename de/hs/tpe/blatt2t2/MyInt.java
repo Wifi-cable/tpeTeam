eba52cb74ac970d233eae8027506063d55434435
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-04-30 16:09:03 +0200

    implemented interface comparable

--- de/hs/tpe/blatt2t2/MyInt.java
+++ de/hs/tpe/blatt2t2/MyInt.java
@@ -1,6 +1,6 @@
 package de.hs.tpe.blatt2t2;
 
-public class MyInt {
+public class MyInt implements Comparable<MyInt> {
 	int value;
 	public MyInt(int value){
 		this.value=value;
@@ -8,6 +8,19 @@ public class MyInt {
 	
 	int getValue(){
 		return value;
+	}
+
+	public int compareTo(MyInt o) {
+		int ret=0;
+		if (value <((MyInt)o).value ){
+	
+			ret=-1;
+		}
+		else if((value >((MyInt)o).value )){
+			ret=1;
+		}
+	
+		 return ret;
 	}
 
 }
--- de/hs/tpe/blatt2t2/MyString.java
+++ de/hs/tpe/blatt2t2/MyString.java
@@ -8,4 +8,10 @@ public class MyString {
 	public String getContent(){
 		return content;
 }
+
+	public int compareTo(MyString o) {
+
+		String st=o.getContent();
+		return content.compareTo(st);
+	}
 }
