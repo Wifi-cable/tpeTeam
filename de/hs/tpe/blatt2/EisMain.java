955a13a5aa20b232b6a05932c12e69678af453b4
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-04-30 18:24:35 +0200

    clean up main method in EisMain

--- de/hs/tpe/blatt2/EisMain.java
+++ de/hs/tpe/blatt2/EisMain.java
@@ -4,10 +4,9 @@ class EisMain {
 	public static void main(String[] args) {
 	System.out.println("aufgabe1 konkrete klassen Eis, und Eisdiele");	
 	Eisdiele venezia= new Eisdiele();
-	System.out.println("spagettieis");
+	venezia.speisekate();
 	venezia.bestellen("spagettieis");
-	//venezia.begruessen();
-	//venezia.kassieren();
+	
 		
 	}
 }
