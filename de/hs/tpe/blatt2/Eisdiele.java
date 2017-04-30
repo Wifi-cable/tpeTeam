c7a5639c04c34f7c0f1f17d6061664e653ae9781
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-04-30 18:22:16 +0200

    add comments, trimm dead code on Eisdiele (concrete)

--- de/hs/tpe/blatt2/Eisdiele.java
+++ de/hs/tpe/blatt2/Eisdiele.java
@@ -1,77 +1,39 @@
 package de.hs.tpe.blatt2;
-//import java.util.*;
+
 class Eisdiele {
 
 	public Eisdiele() {
-		// TODO Auto-generated constructor stub
 	}
 
 	protected Eis myeis;
-	//SpagettiEis mySpagetti= new SpagettiEis();
-	//Eis myEis= new Eis();
-	//public void bestellen(String typ){	// prepares the ice cream sunday	
-		// make the type 
-	//bestellen();
-//	}
+
 	
 	public void bestellen(String bestellung){
 			begruessen();
 			
 			 myeis=new SpagettiEis(bestellung);
-			System.out.println(myeis.getClass());
-			System.out.println(myeis.preis);
-			if (myeis.preis==0.0){
+			System.out.println("sie wollten ein "+myeis.getClass());
+		
+			if (myeis.preis==0.0){//if checks weather fields are uninitiated. (if object does not exist this happens)
 				System.out.println(myeis.preis);
 				System.out.println(myeis.Eisbecher);
-				
+				//appologises because something obviously went wrong
 				endschuldigen();
 			}
-			//Eis	e=this.bestellen(bestellung);	// will proff so, funst aber net weil constructor noch  nicht string nimmt
-			else{
-			System.out.println("in bestellen sorte="+myeis.Eisbecher);
-		//	System.out.println("in bestellen sorte="+mySpagetti.Eisbecher); geht
+			else{//the fields of the subclass are initiated, a correct string was endered. 
 			kassieren();
 			verabschieden();
-			
-			//endschuldigen();
-		
 			}
 	}
-//		Scanner input=new Scanner(System.in);
-//		System.out.println("wir haben");
-//		speisekate();
-//		Eis myEis= new Eis();
-//		begruessen();
-//		String bestellung= input.nextLine();	// muss dann in die methode oben. das macht so keinen sinn
-//		//myEis.rezept(bestellung);
-//		
-//		if (myEis.Eisbecher==null){
-//			endschuldigen();
-//			}
-//		else{
-//			myEis.vorbereiten();
-//			myEis.fuellen();
-//			myEis.dekorieren();
-//			kassieren();
-//		}
-//		verabschieden();
-//		
-//		input.close();
-//	
-//	
-//		return null;	// i have no clue why this is string
-//	}
-//	
 	
 	void begruessen(){	// outputt method
-		
 		System.out.println("Guten tag welchen Eisbecher möchten sie bestellen?");
 	}
 	
 
-	void kassieren(){	// outputmethod
+	void kassieren(){	// output method
 		double kosten= myeis.preis;
-		System.out.println( "ein "+myeis.Eisbecher+" Das macht"+kosten+"€ bitte.");
+		System.out.println( "ein "+myeis.Eisbecher+" Das macht "+kosten+"€ bitte. ");
 	}
 	void verabschieden(){//output method
 		System.out.println("Aufwidersehen lassen sie sich ihr Eis schmecken");
