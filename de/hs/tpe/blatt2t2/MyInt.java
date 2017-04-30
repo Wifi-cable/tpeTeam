85b75cec457f6282423e224d6f30200ae42142cc
wifi-cable <Wifi-cable@users.noreply.github.com>
2017-04-30 12:49:50 +0200

    Classes MyInt and MyString added

---
+++ de/hs/tpe/blatt2t2/MyInt.java
@@ -0,0 +1,13 @@
+package de.hs.tpe.blatt2t2;
+
+public class MyInt {
+	int value;
+	public MyInt(int value){
+		this.value=value;
+	}
+	
+	int getValue(){
+		return value;
+	}
+
+}
---
+++ de/hs/tpe/blatt2t2/MyString.java
@@ -0,0 +1,11 @@
+package de.hs.tpe.blatt2t2;
+
+public class MyString {
+	String content;
+	public MyString(String text){
+		this.content=text;
+	}
+	public String getContent(){
+		return content;
+}
+}
